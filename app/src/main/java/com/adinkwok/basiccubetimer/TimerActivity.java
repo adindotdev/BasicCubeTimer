a5776a4012c392206d44f65ed8fafad9959408fb
Adin Kwok <adin.kwok@carbonrom.org>
2018-08-28 01:48:20 -0400

    Add tap timer functionality

--- app/src/main/java/com/adinkwok/basiccubetimer/TimerActivity.java
+++ app/src/main/java/com/adinkwok/basiccubetimer/TimerActivity.java
@@ -1,13 +1,49 @@
 package com.adinkwok.basiccubetimer;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class TimerActivity extends AppCompatActivity {
+    private static final int sRefreshRate = 50;
+    private TapTimer mTapTimer;
+    private TextView mTimerText;
+
+    private final Handler handler = new Handler();
+
+    private final View.OnClickListener mTimerClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            mTapTimer.tap();
+            updateTimerText();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
+
+        mTapTimer = new TapTimer();
+        View rootView = ((ViewGroup) this
+                .findViewById(android.R.id.content)).getChildAt(0);
+        rootView.setOnClickListener(mTimerClickListener);
+        mTimerText = findViewById(R.id.time_text);
+        updateTimerText();
+    }
+
+    private void updateTimerText() {
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mTimerText.setText(mTapTimer.getFormattedTime());
+                if (mTapTimer.getRunning()) {
+                    handler.postDelayed(this, sRefreshRate);
+                }
+            }
+        }, sRefreshRate);
     }
 }
