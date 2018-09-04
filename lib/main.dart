e79ec6234828c5b78ce67830dec9b2158495d598
Adin Kwok <adin.kwok@carbonrom.org>
2018-09-04 15:55:18 -0700

    Run in fullscreen and organize

--- lib/main.dart
+++ lib/main.dart
@@ -3,24 +3,18 @@ import 'dart:core';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(new BasicCubeTimer());
 
 class BasicCubeTimer extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIOverlays([]);
     return new MaterialApp(
       title: 'Basic Cube Timer',
+      debugShowCheckedModeBanner: false,
       theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: new BasicCubeTimerHome(title: 'Basic Cube Timer'),
@@ -30,15 +24,6 @@ class BasicCubeTimer extends StatelessWidget {
 
 class BasicCubeTimerHome extends StatefulWidget {
   BasicCubeTimerHome({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -101,7 +86,7 @@ class _BasicCubeTimerState extends State<BasicCubeTimerHome> {
     return _formatTime(_stopwatch.elapsedMilliseconds);
   }
 
-  int _getBestTime() {
+  int _getBestSingle() {
     if (_listOfTimes.isNotEmpty) {
       return _listOfTimes.reduce(min);
     }
@@ -115,7 +100,7 @@ class _BasicCubeTimerState extends State<BasicCubeTimerHome> {
     return null;
   }
 
-  int _getLastTime() {
+  int _getLastSingle() {
     if (_listOfTimes.length >= 2) {
       if (_stopwatch.isRunning) {
         return _listOfTimes.last;
@@ -170,123 +155,95 @@ class _BasicCubeTimerState extends State<BasicCubeTimerHome> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return new WillPopScope(
       onWillPop: _onWillPop,
-      child: new Scaffold(
-        appBar: new AppBar(
-          // Here we take the value from the MyHomePage object that was created
-          // by the App.build method, and use it to set our appbar title.
-          title: new Text(widget.title),
-        ),
-        body: new GestureDetector(
-          onTap: _tapTimer,
-          child: new Scaffold(
-            body: new Center(
-              // Center is a layout widget. It takes a single child and positions
-              // it in the middle of the parent.
-              child: new Column(
-                // Column is also layout widget. It takes a list of children and
-                // arranges them vertically. By default, it sizes itself to fit
-                // its children horizontally, and tries to be as tall as its
-                // parent.
-                //
-                // Invoke "debug paint" (press "p" in the console where you ran
-                // "flutter run", or select "Toggle Debug Paint" from the Flutter
-                // tool window in IntelliJ) to see the wireframe for each widget.
-                //
-                // Column has various properties to control how it sizes itself
-                // and how it positions its children. Here we use
-                // mainAxisAlignment to center the children vertically; the main
-                // axis here is the vertical axis because Columns are vertical
-                // (the cross axis would be horizontal).
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  new Text(
-                    _getDisplayTime(),
-                    style: Theme.of(context)
-                        .textTheme
-                        .display1
-                        .apply(fontSizeFactor: _displayTimeFontSize),
-                  ),
-                  new Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      new Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: <Widget>[
-                          new Text(
-                            "Best time:",
-                            style: Theme.of(context)
-                                .textTheme
-                                .display1
-                                .apply(fontSizeFactor: _statsFontSize),
-                          ),
-                          new Text(
-                            "Last time:",
-                            style: Theme.of(context)
-                                .textTheme
-                                .display1
-                                .apply(fontSizeFactor: _statsFontSize),
-                          ),
-                          new Text(
-                            "Best Ao5:",
-                            style: Theme.of(context)
-                                .textTheme
-                                .display1
-                                .apply(fontSizeFactor: _statsFontSize),
-                          ),
-                          new Text(
-                            "Last Ao5:",
-                            style: Theme.of(context)
-                                .textTheme
-                                .display1
-                                .apply(fontSizeFactor: _statsFontSize),
-                          ),
-                        ],
-                      ),
-                      new Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: <Widget>[
-                          new Text(
-                            _formatTime(_getBestTime()),
-                            style: Theme.of(context)
-                                .textTheme
-                                .display1
-                                .apply(fontSizeFactor: _statsFontSize),
-                          ),
-                          new Text(
-                            _formatTime(_getLastTime()),
-                            style: Theme.of(context)
-                                .textTheme
-                                .display1
-                                .apply(fontSizeFactor: _statsFontSize),
-                          ),
-                          new Text(
-                            _formatTime(_getBestAvgOfFive()),
-                            style: Theme.of(context)
-                                .textTheme
-                                .display1
-                                .apply(fontSizeFactor: _statsFontSize),
-                          ),
-                          new Text(
-                            _formatTime(_getLastAvgOfFive()),
-                            style: Theme.of(context)
-                                .textTheme
-                                .display1
-                                .apply(fontSizeFactor: _statsFontSize),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                ],
-              ),
+      child: new GestureDetector(
+        onTap: _tapTimer,
+        child: new Scaffold(
+          appBar: new AppBar(title: new Text(widget.title)),
+          body: new Center(
+            child: new Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new Text(
+                  _getDisplayTime(),
+                  style: Theme.of(context)
+                      .textTheme
+                      .display1
+                      .apply(fontSizeFactor: _displayTimeFontSize),
+                ),
+                new Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    new Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        new Text(
+                          "Best single:",
+                          style: Theme.of(context)
+                              .textTheme
+                              .display1
+                              .apply(fontSizeFactor: _statsFontSize),
+                        ),
+                        new Text(
+                          "Last single:",
+                          style: Theme.of(context)
+                              .textTheme
+                              .display1
+                              .apply(fontSizeFactor: _statsFontSize),
+                        ),
+                        new Text(
+                          "Best Ao5:",
+                          style: Theme.of(context)
+                              .textTheme
+                              .display1
+                              .apply(fontSizeFactor: _statsFontSize),
+                        ),
+                        new Text(
+                          "Last Ao5:",
+                          style: Theme.of(context)
+                              .textTheme
+                              .display1
+                              .apply(fontSizeFactor: _statsFontSize),
+                        ),
+                      ],
+                    ),
+                    new Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: <Widget>[
+                        new Text(
+                          _formatTime(_getBestSingle()),
+                          style: Theme.of(context)
+                              .textTheme
+                              .display1
+                              .apply(fontSizeFactor: _statsFontSize),
+                        ),
+                        new Text(
+                          _formatTime(_getLastSingle()),
+                          style: Theme.of(context)
+                              .textTheme
+                              .display1
+                              .apply(fontSizeFactor: _statsFontSize),
+                        ),
+                        new Text(
+                          _formatTime(_getBestAvgOfFive()),
+                          style: Theme.of(context)
+                              .textTheme
+                              .display1
+                              .apply(fontSizeFactor: _statsFontSize),
+                        ),
+                        new Text(
+                          _formatTime(_getLastAvgOfFive()),
+                          style: Theme.of(context)
+                              .textTheme
+                              .display1
+                              .apply(fontSizeFactor: _statsFontSize),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ],
             ),
           ),
         ),
